fe659120f1c930b5c6c413b68389bafc458802c3
styp3r <stypertypes.com>
2022-09-08 18:18:29 +0530

    Creates navigation block with google icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,12 @@ function Home() {
 
                 <div className="homeNavContainer">
                     <div className="homeNavigation">
-                        
+                        <div className="navLinkBlock">
+                            <p className = "navLink">Accounts <span class="material-icons">library_books</span></p>
+                            <p className = "navLink">Vendors <span class="material-icons">groups</span></p>
+                            <p className = "navLink">Licenses <span class="material-icons">file_present</span></p>
+                            <p className = "navLink">Insights <span class="material-icons">insights</span></p>
+                        </div>
                     </div>
                 </div>
 
