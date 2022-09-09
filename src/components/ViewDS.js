bcf39b44997365b6182ea867534d177ed58f5886
styp3r <stypertypes.com>
2022-09-09 11:02:39 +0530

    Fetch row based on the input date

--- src/components/ViewDS.js
+++ src/components/ViewDS.js
@@ -3,15 +3,22 @@ import React, { useState } from 'react';
 
 function ViewDS() {
 
+    const [fetchDate, setFetchDate] = useState("");
     const [dailySheetFig, setDailySheetFig] = useState("");
 
     async function getData() {
-        const { data, error } = await supabase.from('2022').select();
+        const { data, error } = await supabase
+            .from('2022')
+            .select('*')
+            .eq('date', fetchDate)
 
         if (data) {
             for (let i in data) {
+                if(!data[i].date){
+                    console.log(data[i].date)
+                }
                 //console.log(data[i].date, data[i].sale, data[i].gst, data[i].discount, data[i].creditCard, data[i].digital)
-                setDailySheetFig(data[i].date +" "+ data[i].sale +" "+ data[i].gst +" "+ data[i].discount +" "+ data[i].creditCard +" "+ data[i].digital);
+                setDailySheetFig(data[i].date + " " + data[i].sale + " " + data[i].gst + " " + data[i].discount + " " + data[i].creditCard + " " + data[i].digital);
             }
         }
 
@@ -23,6 +30,7 @@ function ViewDS() {
     return (
         <div id="viewDSPage">
             <h1>View Daily Sheet</h1>
+            <input onChange={(e) => setFetchDate(e.target.value)} type="date" />
             <p>{dailySheetFig}</p>
             <button onClick={() => getData()}>Get Data</button>
         </div>
