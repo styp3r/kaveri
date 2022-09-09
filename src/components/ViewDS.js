64f3b61dfdf4ddfee200ba6e5375339259fdfae5
styp3r <stypertypes.com>
2022-09-09 08:49:28 +0530

    Successfully fetches data from database and displays in console.

--- src/components/ViewDS.js
+++ src/components/ViewDS.js
@@ -1,17 +1,24 @@
 import { supabase } from '../supabaseClient';
+import React from 'react';
 
 function ViewDS() {
 
-    async function viewList() {
-        const { data, error } = await supabase
-        .from('2022')
-        .select();
+    async function getData() {
+        const { data, error } = await supabase.from('2022').select('*');
+
+        if(data){
+            console.log(data[0].date, data[0].sale, data[0].gst, data[0].discount, data[0].creditCard, data[0].digital)
+        }
+
+        if(error){
+            console.log(error);
+        }
     }
 
     return (
         <div id="viewDSPage">
             <h1>View Daily Sheet</h1>
-            {viewList()}
+            <button onClick={() => getData()}>Get Data</button>
         </div>
     );
 }
