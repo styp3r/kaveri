a1421b9159b1974108f49637f10e9c475da13d81
styp3r <stypertypes@gmail.com>
2022-09-19 21:45:22 +0530

    Redesigns upload and edit input field sections

--- src/components/DSInputFields.js
+++ src/components/DSInputFields.js
@@ -42,29 +42,36 @@ function DSInputFields(props) {
         <div className="inputFieldsForUploadBlock">
             <h3>{props.mode} Daily Sheet Parameters</h3>
             <div className="inputBlock">
-                <input onChange={(e) => { setIsEnabled(true); setDate(e.target.value) }} className="dateInputBox" id="dateUpload" type="date" required />
-                {/*<input onChange={(e) => setShop(e.target.value)} id="shopUpload" type="text" placeholder="Shop" required />*/}
-                <select id="shopUpload" onChange={(e) => setShop(e.target.value)} required>
-                    <option value="none" selected disabled hidden>Select Branch</option>
-                    <option value="BTM">BTM</option>
-                    <option value="EC2">EC2</option>
-                    <option value="JBN">JBN</option>
-                    <option value="VTR">VTR</option>
-                    <option value="HRU">HRU</option>
-                    <option value="KTR">KTR</option>
-                    <option value="DDK">DDK</option>
-                    <option value="MLL">MLL</option>
-                    <option value="ORR">ORR</option>
-                </select>
-                <input onChange={(e) => setSale(e.target.value)} id="saleUpload" type="number" placeholder="Total Net" required />
-                <input onChange={(e) => setGST(e.target.value)} id="gstUpload" type="number" placeholder="Total Tax" required />
-                <input onChange={(e) => setDiscount(e.target.value)} id="discountUpload" type="number" placeholder="Partner Discount" required />
-                <input onChange={(e) => setPartnerPending(e.target.value)} id="pPendingUpload" type="number" placeholder="Partner Pending" required />
-                <input onChange={(e) => setCash(e.target.value)} id="cashUpload" type="number" placeholder="Cash" required />
-                <input onChange={(e) => setCreditCard(e.target.value)} id="creditCardUpload" type="number" placeholder="Credit Card Sales" required />
-                <input onChange={(e) => setDigital(e.target.value)} id="digitalUpload" type="number" placeholder="Digital Payment Sales" required />
-                <button onClick={() => handleUpload()} id="uploadDSBtn">Upload</button>
+                <div className="inputBlockLeft">
+                    <input onChange={(e) => { setIsEnabled(true); setDate(e.target.value) }} id="dateUpload" type="date" required />
+                    <select id="shopUpload" onChange={(e) => setShop(e.target.value)} required>
+                        <option value="none" selected disabled hidden>Select Branch</option>
+                        <option value="BTM">BTM</option>
+                        <option value="EC2">EC2</option>
+                        <option value="JBN">JBN</option>
+                        <option value="VTR">VTR</option>
+                        <option value="HRU">HRU</option>
+                        <option value="KTR">KTR</option>
+                        <option value="DDK">DDK</option>
+                        <option value="MLL">MLL</option>
+                        <option value="ORR">ORR</option>
+                    </select>
+                </div>
+
+                <div className="inputBlockMid">
+                    <input onChange={(e) => setSale(e.target.value)} id="saleUpload" type="number" placeholder="Total Net" required />
+                    <input onChange={(e) => setGST(e.target.value)} id="gstUpload" type="number" placeholder="Total Tax" required />
+                    <input onChange={(e) => setCash(e.target.value)} id="cashUpload" type="number" placeholder="Cash" required />
+                    <input onChange={(e) => setCreditCard(e.target.value)} id="creditCardUpload" type="number" placeholder="Credit Card Sales" required />
+                </div>
+
+                <div className="inputBlockRight">
+                    <input onChange={(e) => setDiscount(e.target.value)} id="discountUpload" type="number" placeholder="Partner Discount" required />
+                    <input onChange={(e) => setPartnerPending(e.target.value)} id="pPendingUpload" type="number" placeholder="Partner Pending" required />
+                    <input onChange={(e) => setDigital(e.target.value)} id="digitalUpload" type="number" placeholder="Digital Payment Sales" required />
+                </div>
             </div>
+            <button onClick={() => handleUpload()} id="uploadDSBtn">Upload</button>
         </div>
     );
 }
